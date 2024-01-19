5da4a2891336ef1cedfb447e812e0a3190ee11e9
Rafal Kochanecki <rafal.kochanecki@gmail.com>
2024-01-19 21:00:43 +0100

    chore: add comments to hook

--- src/hooks/useLocalStorage/useLocalStorage.ts
+++ src/hooks/useLocalStorage/useLocalStorage.ts
@@ -1,30 +1,53 @@
 import { useState, useEffect } from 'react'
 
-export const useLocalStorage = (key: string, initialValue: unknown) => {
+export const useLocalStorage = (
+  /*
+    *
+    key variable under which the value is saved in localStorage
+  */
+  key: string,
+  /*
+    *
+    initialValue is the initial of useState
+  */
+  initialValue: unknown
+) => {
+  /*
+    *
+    flag variable if it run in server or client mode
+  */
   const isLocalStorageAvailable = typeof window !== 'undefined'
-
-  const [isFirstRender, setIsFirstRender] = useState(true)
-  const [state, setState] = useState(() => getInitialValue(true))
-
-  function getInitialValue (isFirstRender: boolean) {
+  /*
+    *
+    store with initial value returned from getInitialValue fn
+  */
+  const [state, setState] = useState(getInitialValue)
+  /*
+    *
+    Fn
+    *
+    when run on the client side, it returns the retrieved localStorage value based on key variable
+    *
+    when run in the server side, it returns initialValue variable
+  */
+  function getInitialValue () {
     if (isLocalStorageAvailable) {
-      const storedValue = isFirstRender ? initialValue : localStorage.getItem(key)
-      return JSON.parse(storedValue as string)
+      const storedValue = localStorage.getItem(key)
+      return storedValue ? JSON.parse(storedValue) : initialValue
     } else {
       return initialValue
     }
   }
-
+  /*
+    *
+    side effect to save state in localStorage
+  */
   useEffect(() => {
-    if (isFirstRender) setIsFirstRender(false)
-    // mount-only
-  }, [])
-
-  useEffect(() => {
-    if (isLocalStorageAvailable) {
-      localStorage.setItem(key, JSON.stringify(state))
-    }
+    localStorage.setItem(key, JSON.stringify(state))
   }, [key, state, isLocalStorageAvailable])
-
+  /*
+    *
+    return state value and modifying fn
+  */
   return [state, setState]
 }
